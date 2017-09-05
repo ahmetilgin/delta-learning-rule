51ea52ae62be1160d5beabb9c4edc7e820241af7
xiaozhuai <798047000@qq.com>
2017-09-05 11:00:48 +0800

    This code very fast

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
 #include "headers/siniragi.h"
 using namespace std;
-
-int main()
-{
-    SinirAgi deltaAgi;
-
-
-    deltaAgi.araKatman[0].agirliklariEkrandaGoster();
-    deltaAgi.araKatman[1].agirliklariEkrandaGoster();
-    deltaAgi.cikisKatmani.agirliklariEkrandaGoster();
-    deltaAgi.Sinif[0].beklenenDeger = 1;
-    deltaAgi.Sinif[1].beklenenDeger = -1;
-
-
-    return 0;
-}
+int main(){SinirAgi deltaAgi;deltaAgi.araKatman[0].agirliklariEkrandaGoster();deltaAgi.araKatman[1].agirliklariEkrandaGoster();deltaAgi.cikisKatmani.agirliklariEkrandaGoster();deltaAgi.Sinif[0].beklenenDeger = 1;deltaAgi.Sinif[1].beklenenDeger = -1;return 0;}
